611e259f7e9391ec7ab3af00a0ca28f7093f061f
Dalian Banks <dbanks@debtcleanse.com>
2020-01-13 20:09:33 -0600

    base app config file

--- app.config.js
+++ app.config.js
@@ -3,18 +3,18 @@
 const config = {
 	env: process.env.NODE_ENV || 'development',
 	production: {
-		zomato_api_url: 'https://developers.zomato.com/api/v2.1/',
-		zomato_api_key: '4d698083d03a6a306cc2a44fa13b23ee',
-		amplify_aws_ak_id: 'AKIA3GN3H7OXW2EMBAPK',
-		amplify_aws_ak_secret: '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'
+		zomato_api_url: '',
+		zomato_api_key: '',
+		amplify_aws_ak_id: '',
+		amplify_aws_ak_secret: ''
 	},
 	development: {
-		zomato_api_url: 'https://developers.zomato.com/api/v2.1/',
-		zomato_api_key: '4d698083d03a6a306cc2a44fa13b23ee',
-		amplify_aws_ak_id: 'AKIA3GN3H7OXW2EMBAPK',
-		amplify_aws_ak_secret: '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',
-		sls_aws_ak_id: 'AKIA3GN3H7OX43TSJ37H',
-		sls_aws_ak_secret: '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'
+		zomato_api_url: '',
+		zomato_api_key: '',
+		amplify_aws_ak_id: '',
+		amplify_aws_ak_secret: '',
+		sls_aws_ak_id: '',
+		sls_aws_ak_secret: ''
 	},
 	get current() {
 		return this[this.env];
